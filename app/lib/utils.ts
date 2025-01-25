f51b83470b5bdb4c1461e840e38bed668f0d843b
unknown <ejbiasi@hotmail.com>
2025-01-25 01:23:54 -0300

    chore: add verifications and create link

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,5 +1,15 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function sanitizeLink(link?: string) {
+  if (!link) return "";
+
+  return link
+    .replace(/\s/g, "")
+    .replace(/[!@#$%^&*()_+\-=\[\]{};':"\\|,ˆ.<>\/?]+/, "")
+    .toLocaleLowerCase();
 }
