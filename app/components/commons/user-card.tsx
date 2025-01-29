a1712f879f7d7e8efb7c05db8b07402df5d9fb6b
unknown <ejbiasi@hotmail.com>
2025-01-28 23:03:49 -0300

    chore: add create-project integration

--- app/components/commons/user-card.tsx
+++ app/components/commons/user-card.tsx
@@ -8,8 +8,8 @@ export default function UserCard() {
     <div className="w-[348px] flex flex-col gap-5 items-center p-5 border border-white border-opacity-10 bg-[#121212] rounded-3xl text-white">
       <div className="size-48">
         <img
-          src="/me1.jpg"
-          alt="André Dev"
+          src="/emiliobiasi.jpg"
+          alt="Emílio Biasi"
           className="rounded-full object-cover w-full h-full"
         />
       </div>
