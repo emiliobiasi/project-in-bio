583d116014d4f9fef5be93ad7164cab1d5ce6127
unknown <ejbiasi@hotmail.com>
2025-02-03 11:30:30 -0300

    chore: add mipanel key

--- app/lib/mixpanel.ts
+++ app/lib/mixpanel.ts
@@ -1,6 +1,6 @@
 import Mixpanel from "mixpanel";
 
-const mixpanelEvent = Mixpanel.init("YOUR-MIXPANEL-TOKEN");
+const mixpanelEvent = Mixpanel.init("3c392e84b17d16f075d87836b6f026f5");
 
 export function trackServerEvent(eventName: string, properties: any) {
   if (process.env.NODE_ENV === "development") return;
