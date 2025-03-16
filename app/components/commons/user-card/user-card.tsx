d66f9f8f127993e452dbb68729c1c9fdb9011bb3
unknown <ejbiasi@hotmail.com>
2025-03-15 23:06:22 -0300

    fix: placeholder

--- app/components/commons/user-card/user-card.tsx
+++ app/components/commons/user-card/user-card.tsx
@@ -11,20 +11,31 @@ import { getDownloadURLFromPath } from "@/app/lib/firebase";
 export default async function UserCard({
   profileData,
   isOwner,
+  isLandingPage,
 }: {
   profileData?: ProfileData;
   isOwner?: boolean;
+  isLandingPage?: boolean;
 }) {
   const icons = [Github, Instagram, Linkedin, Twitter, Plus];
+
+  const profileImage = isLandingPage
+    ? "/emiliobiasi1.png"
+    : (await getDownloadURLFromPath(profileData?.imagePath)) ||
+      "/profilepic-placeholder.png";
+
+  const profileName = isLandingPage
+    ? "Emílio Biasi"
+    : profileData?.name || "Seu Nome";
+  const profileDescription = isLandingPage
+    ? "Engenheiro de Software"
+    : profileData?.description || "Descrição breve sobre você";
 
   return (
     <div className="w-[348px] flex flex-col gap-5 items-center p-5 border border-white border-opacity-10 bg-[#121212] rounded-3xl text-white">
       <div className="size-48">
         <img
-          src={
-            (await getDownloadURLFromPath(profileData?.imagePath)) ||
-            "/emiliobiasi1.jpg"
-          }
+          src={profileImage}
           alt="Profile image"
           className="rounded-full object-cover w-full h-full"
         />
@@ -32,13 +43,11 @@ export default async function UserCard({
       <div className="flex flex-col gap-2 w-full">
         <div className="flex items-center gap-2">
           <h3 className="text-3xl font-bold min-w-0 overflow-hidden">
-            {profileData?.name || "Emílio Biasi"}
+            {profileName}
           </h3>
           {isOwner && <EditUserCard profileData={profileData} />}
         </div>
-        <p className="opacity-40">
-          {profileData?.description || "Engenheiro de Software"}
-        </p>
+        <p className="opacity-40">{profileDescription}</p>
       </div>
       <div className="flex flex-col gap-2 w-full">
         <span className="uppercase text-xs font-medium">Links</span>
